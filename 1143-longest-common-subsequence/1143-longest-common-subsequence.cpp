5b49cffbe2fe582f05ca0878901d22595d1f9086
Manya <123743851+manya16102003@users.noreply.github.com>
2023-10-02 22:22:47 +0530

    Time: 48 ms (62.08%), Space: 24.8 MB (11.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    int solve(string &text1, string &text2, int i , int j,vector<vector<int>>&dp)
+    // int solve(string &text1, string &text2, int i , int j,vector<vector<int>>&dp)
+    // {
+    //     if(i==text1.length()) return 0;
+    //     if(j==text2.length()) return 0;
+    //     int ans=0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(text1[i]==text2[j])
+    //     {
+    //         ans=1+solve(text1 , text2,i+1,j+1,dp);
+    //     }
+    //     else
+    //     {
+    //         ans=0+max(solve(text1,text2, i+1 ,j,dp),solve(text1,text2, i ,j+1,dp));
+    //     }
+    //     dp[i][j]=ans;
+    //     return dp[i][j];
+    // }
+    int solvetab(string &text1, string &text2)
     {
-        if(i==text1.length()) return 0;
-        if(j==text2.length()) return 0;
-        int ans=0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j])
+       vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        for(int i=text1.length()-1;i>=0;i--)
         {
-            ans=1+solve(text1 , text2,i+1,j+1,dp);
+            for(int j=text2.length()-1;j>=0;j--)
+            {
+                 int ans=0;
+            
+            if(text1[i]==text2[j])
+            {
+                ans=1+dp[i+1][j+1];
+            }
+            else
+            {
+                ans=0+max(dp[i+1] [j],dp[i][j+1]);
+            }
+                 dp[i][j]=ans;
+            }
         }
-        else
-        {
-            ans=0+max(solve(text1,text2, i+1 ,j,dp),solve(text1,text2, i ,j+1,dp));
-        }
-        dp[i][j]=ans;
-        return dp[i][j];
+        return dp[0][0];
+        
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int i=0;
-        int j=0;
-        vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
-        return solve(text1,text2, i ,j,dp);
+        // int i=0;
+        // int j=0;
+        // vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
+        return solvetab(text1,text2);
         
     }
 };
