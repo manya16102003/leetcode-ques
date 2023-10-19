cd06ed70077a8c6427eae67df830ed286158a484
Manya <123743851+manya16102003@users.noreply.github.com>
2023-10-19 17:05:03 +0530

    Time: 10 ms (80.72%), Space: 10.7 MB (65.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -30,12 +30,30 @@ public:
         return dp[0][0];
         
     }
+    int solvedpwithbinary(vector<int>& nums)
+    {
+        if(nums.size()==0) return 0;
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>ans.back()) ans.push_back(nums[i]);
+            else
+            {
+                // override
+                //find index of just bada element
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
+        }
+        return ans.size();
+    }
     int lengthOfLIS(vector<int>& nums) {
         int curr=0;
         int prev=-1;
         // int n=nums.size();
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return solve(nums,curr,prev,dp);
-        return solvetab(nums);
+        return solvedpwithbinary(nums);
     }
 };
