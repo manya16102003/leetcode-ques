e4a2d2413f94970600111b825cd0229f3d3f90e8
Manya <123743851+manya16102003@users.noreply.github.com>
2023-10-30 13:26:12 +0530

    Time: 15 ms (6.08%), Space: 15.5 MB (20.13%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+int solvetb(vector<int>& prices,int buy,vector<vector<int>>&dp,int i)
+{
+    if(i>=prices.size()) return 0;
+    int profit=0;
+    if(dp[i][buy]!=-1) return dp[i][buy];
+    if(buy)
+    {
+        int stockprofit=-prices[i]+solvetb(prices,0,dp,i+1);
+        int skip=solvetb(prices,1,dp,i+1);
+        profit=max(stockprofit,skip); 
+    }
+    else
+    {
+       int sellprofit=prices[i]+solvetb(prices,1,dp,i+1);
+        int skip=solvetb(prices,0,dp,i+1);
+        profit=max(sellprofit,skip);  
+    }
+    dp[i][buy]=profit;
+    return dp[i][buy];
+}
+
+int solvebu(vector<int>& prices,vector<vector<int>>&dp)
+{
+    for(int i=prices.size()-1;i>=0;--i)
+    {
+        for(int buy=0;buy<2;buy++)
+        {
+        int profit=0;
+        if(buy)
+        {
+            int stockprofit=-prices[i]+dp[i+1][0];
+            int skip=dp[i+1][1];
+            profit=max(stockprofit,skip); 
+        }
+        else
+        {
+        int sellprofit=prices[i]+dp[i+1][1];
+            int skip=dp[i+1][0];
+            profit=max(sellprofit,skip);  
+        }
+        dp[i][buy]=profit;
+        
+        }
+    }
+    
+   return dp[0][1];
+   
+}
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>dp(prices.size()+1,vector<int>(2,0));
+      return solvebu(prices,dp);
+    }
+};
