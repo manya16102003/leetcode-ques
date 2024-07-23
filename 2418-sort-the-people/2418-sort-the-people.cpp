c13d4b8fd613c27d2fdfa27314259e285a134090
Manya <123743851+manya16102003@users.noreply.github.com>
2024-07-23 13:59:01 +0530

    Time: 27 ms (70.62%), Space: 27.4 MB (38.47%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0;i<names.size();i++){
             map.push_back({heights[i],names[i]});
         }
+        //if you wan to sort the map in decreasing order
         sort(map.begin(),map.end(),greater());
         vector<string>ans;
         for(auto x: map) ans.push_back(x.second);
